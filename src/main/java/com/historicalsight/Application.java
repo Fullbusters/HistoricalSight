342fc52658aadadb44209d41d9952616999ff747
Fullbusters <fullbuster0033@gmail.com>
2018-11-12 23:01:56 +0200

    first commit

---
+++ src/main/java/com/historicalsight/Application.java
@@ -0,0 +1,13 @@
+package com.historicalsight;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        SpringApplication.run(com.historicalsight.Application.class, args);
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,70 @@
+INSERT INTO Region(Title) VALUES ("Vinnitsa");
+INSERT INTO Region(Title) VALUES ("Volynsky");
+INSERT INTO Region(Title) VALUES ("Dnipropetrovsk");
+INSERT INTO Region(Title) VALUES ("Donetsk");
+INSERT INTO Region(Title) VALUES ("Zhytomyr");
+INSERT INTO Region(Title) VALUES ("Zakarpatska");
+INSERT INTO Region(Title) VALUES ("Zaporozhye");
+INSERT INTO Region(Title) VALUES ("Ivano-Frankivsk");
+INSERT INTO Region(Title) VALUES ("Kiev");
+INSERT INTO Region(Title) VALUES ("Kirovogradskaya");
+INSERT INTO Region(Title) VALUES ("Lugansk");
+INSERT INTO Region(Title) VALUES ("Lviv");
+INSERT INTO Region(Title) VALUES ("Nikolaev");
+INSERT INTO Region(Title) VALUES ("Odessa");
+INSERT INTO Region(Title) VALUES ("Poltava");
+INSERT INTO Region(Title) VALUES ("Rivne");
+INSERT INTO Region(Title) VALUES ("Sumy");
+INSERT INTO Region(Title) VALUES ("Ternopil");
+INSERT INTO Region(Title) VALUES ("Kharkiv");
+INSERT INTO Region(Title) VALUES ("Kherson");
+INSERT INTO Region(Title) VALUES ("Khmelnitsky");
+INSERT INTO Region(Title) VALUES ("Cherkassy");
+INSERT INTO Region(Title) VALUES ("Chernivtsi");
+INSERT INTO Region(Title) VALUES ("Chernihiv");
+
+
+
+
+
+
+
+
+
+
+INSERT INTO City(Title,Region_id) VALUES ("Ivano-Frankivsk", 8);
+INSERT INTO City(Title,Region_id) VALUES ("Kolomiya", 8);
+INSERT INTO City(Title,Region_id) VALUES ("Kalush", 8);
+INSERT INTO City(Title,Region_id) VALUES ("Kolomiya", 8);
+INSERT INTO City(Title,Region_id) VALUES ("Kosiv", 8);
+INSERT INTO City(Title,Region_id) VALUES ("Doluna", 8);
+INSERT INTO City(Title,Region_id) VALUES ("Yaremcha", 8);
+INSERT INTO City(Title,Region_id) VALUES ("Horodenka", 8);
+INSERT INTO City(Title,Region_id) VALUES ("Tysmenytsia", 8);
+
+
+INSERT INTO Historicalsight(Title,Address,City_id,Info,Image) VALUES ("Collegiate Church of the virgin Mary","Sheptytsky Square, 8",1,"The exact date of construction of the Church is unknown. The founder of the city Andrzej Potocki began to build the walls around the city, at the same time took up the construction of the Roman Catholic parish Church","https://upload.wikimedia.org/wikipedia/commons/3/30/Ukraine-Ivano-Frankivsk-Church_of_Virgin_Mary-3.jpg");
+INSERT INTO Historicalsight(Title,Address,City_id,Info,Image) VALUES ("Jesuit College","Maidan Sheptytsky, 21",1);
+INSERT INTO Historicalsight(Title,Address,City_id,Info,Image) VALUES ("The Armenian Church","6, Armenian street",1);
+INSERT INTO Historicalsight(Title,Address,City_id,Info,Image) VALUES ("Church Of The Jesuits (Cathedral Of The Holy Resurrection)","Sheptytsky Square, 22",1);
+INSERT INTO Historicalsight(Title,Address,City_id,Info,Image) VALUES ("Brewery","Novgorodskaja, 49, 28",1);
+INSERT INTO Historicalsight(Title,Address,City_id,Info,Image) VALUES ("Church of St. Joseph the betrothed (Moore.)","Vovchinetska, 54",1);
+INSERT INTO Historicalsight(Title,Address,City_id,Info,Image) VALUES ("The Austrian Directorate of Railways, later the Polish magistrate (Moore:)","Galitskaya str., 2",1);
+INSERT INTO Historicalsight(Title,Address,City_id,Info,Image) VALUES ("Guildhall","Galitskaya str., 4A",1);
+
+
+
+
+
+
+
+
+
+INSERT INTO City(Title,Region_id) VALUES ("Lviv");
+INSERT INTO City(Title,Region_id) VALUES ("Kyiv");
+
+INSERT INTO Categories(Title) VALUES ("monuments");
+INSERT INTO Categories(Title) VALUES ("parks");
+INSERT INTO Categories(Title) VALUES ("museums");
+
+INSERT INTO Info(Info, Language) VALUES ("aaaa", "english");
